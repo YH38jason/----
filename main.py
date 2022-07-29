a7142e1d41d14e81e5380580141b58a032a85603
YH38jason <102440370+YH38jason@users.noreply.github.com>
2022-07-29 20:04:34 +0800

    -

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #coding=utf-8
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 import json
 import sqlite3
 
@@ -25,12 +25,19 @@ def home():
     for post in posts:
         if post[2] >= ans[2]:
             ans = post
-    
-    if isLogin():
-        return render_template('home.htm', is_login=True, username=\
-        request.cookies.get("userName"), author=ans[3], title=ans[0], like=ans[2])
     con.close()
-    return render_template('home.htm', author=ans[3], title=ans[0], like=ans[2])
+    if len(posts) == 0:
+        no_post = True
+    else:
+        no_post = False
+    try:
+        if isLogin():
+            return render_template('home.htm', is_login=True, username=\
+            request.cookies.get("userName"), author=ans[3], title=ans[0], like=ans[2], no_post=no_post)
+        return render_template('home.htm', author=ans[3], title=ans[0], like=ans[2], no_post=no_post)
+    except IndexError:
+        return render_template('home.htm', is_login=isLogin(), no_post=no_post, username=request.cookies.get("userName"))
+
 
 # 登录
 @app.route('/login')
@@ -62,7 +69,7 @@ def login_check():
 
 # 搜索
 @app.route('/s', methods=['GET', 'POST'])
-def searchred():
+def search():
     form = request.form
     if form['keyword'] == '':
         return redirect('/')
@@ -71,6 +78,8 @@ def searchred():
     sql = "SELECT TITLE FROM POSTS WHERE TITLE LIKE ?"
     cur.execute(sql, ('%'+form['keyword']+'%',))
     result = [i[0] for i in cur.fetchall()]
+    if len(result) == 0:
+        return redirect('/')
     results = json.dumps(result)
     return render_template('search.htm', results=results)
     # return str(results)
@@ -119,8 +128,12 @@ def viewPost(title):
     post = cur.fetchall()[0]
     # return str(post)
     con.close()
+    if request.cookies.get('userName') == post[3]:
+        is_author = True
+    else:
+        is_author = False
     return render_template('post.html', title=post[0], content=post[1].replace('\r', '')\
-        , like=post[2], author=post[3])
+        , like=post[2], author=post[3], is_author=is_author)
 
 # 点赞帖子
 @app.route('/postpage/<title>/like')
@@ -130,18 +143,37 @@ def like(title):
     con = sqlite3.connect('data.db')
     cur = con.cursor()
     cur.execute('SELECT ISLIKE FROM USERS WHERE USER_NAME = ?', (request.cookies.get("userName"),))
-    likes = cur.fetchall()[0][0]
-    if title in likes:
-        return redirect(f'/postpage/{title}')
-    else:
+    if len(cur.fetchall()) == 0:
         sql = "UPDATE POSTS SET LIKE=LIKE+1 WHERE TITLE = ?"
         cur.execute(sql, (title,))
         con.commit()
         sql = "UPDATE USERS SET ISLIKE=? WHERE USER_NAME = ?"
-        cur.execute(sql, (likes+title, request.cookies.get("userName")))
+        cur.execute(sql, (title, request.cookies.get("userName")))
         con.commit()
+    else:
+        cur.execute('SELECT ISLIKE FROM USERS WHERE USER_NAME = ?', (request.cookies.get("userName"),))
+        likes = cur.fetchall()[0][0]
+        if title in likes:
+            return redirect(f'/postpage/{title}')
+        else:
+            sql = "UPDATE POSTS SET LIKE=LIKE+1 WHERE TITLE = ?"
+            cur.execute(sql, (title,))
+            con.commit()
+            sql = "UPDATE USERS SET ISLIKE=? WHERE USER_NAME = ?"
+            cur.execute(sql, (title, request.cookies.get("userName")))
+            con.commit()
     con.close()
     return redirect(f'/postpage/{title}')
+
+# 删帖
+@app.route('/postpage/<title>/delete')
+def delete_post(title):
+    con = sqlite3.connect('data.db')
+    cur = con.cursor()
+    cur.execute('DELETE FROM POSTS WHERE TITLE = ?', (title,))
+    con.commit()
+    con.close()
+    return redirect('/')
 
 # 发帖
 @app.route('/posting')
@@ -163,4 +195,4 @@ def posting():
 
 if __name__ == '__main__':
     # 运行
-    app.run(port=2651, debug=True)
+    app.run(port=2651, debug=False)
